b1b2d1b4636c8c1cafbc1d69b5856358bd40e9bd
Paras Kumar <paraskm6812@gmail.com>
2023-08-02 21:25:44 +0530

    feat: add javascript  in navbar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const hamMenu = document.getElementById("ham-menu")
 
+
 hamMenu.addEventListener("click", () => {
     const navbar = document.getElementById("navbar")
     navbar.classList.toggle("nav-toggle")
 })
+
+const navLinks = document.getElementById("nav__links")
+
+navLinks.addEventListener("click", () => {
+    const navbar = document.getElementById("navbar")
+    navbar.classList.toggle("nav-toggle")
+})
+
